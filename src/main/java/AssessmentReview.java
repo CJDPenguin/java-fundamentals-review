4b603aa3132ec58809d70c7997e6bbff1937c8b4
Chris Davis <christopher.j.davis03@gmail.com>
2022-06-29 14:14:49 -0500

    practice

--- src/main/java/AssessmentReview.java
+++ src/main/java/AssessmentReview.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+
 public class AssessmentReview {
+
+    public static int cubed(int num){
+        return (int) Math.pow((double) num, 3);
+    }
+
+    public static int difference (int first, int second){
+        return first - second;
+    }
+
+    public static double difference (double first, double second){
+        return first - second;
+    }
+
+    public static double median(int[] array) {
+        Arrays.sort(array);
+        double median;
+        if (array.length % 2 == 0) {
+            median = (double) (array[array.length/2] + array[array.length/2 - 1])/2;
+        }else {
+            median = array[array.length/2];
+        }
+        return median;
+    }
 
 }
--- src/main/test-methods/TestDifferenceMethods.java
+++ src/main/test-methods/TestDifferenceMethods.java
@@ -2,18 +2,18 @@ public class TestDifferenceMethods {
 
     public static void main(String[] args) {
 
-        // TEST DIFFERENCE (INT)
+         TEST DIFFERENCE (INT)
 
-//        System.out.println(5 == AssessmentReview.difference(10, 5));
-//        System.out.println(100 == AssessmentReview.difference(150, 50));
-//        System.out.println(-2 == AssessmentReview.difference(5, 7));
+        System.out.println(5 == AssessmentReview.difference(10, 5));
+        System.out.println(100 == AssessmentReview.difference(150, 50));
+        System.out.println(-2 == AssessmentReview.difference(5, 7));
 
 
-        // TEST DIFFERENCE (DOUBLE)
+         TEST DIFFERENCE (DOUBLE)
 
-//        System.out.println(5.5 == AssessmentReview.difference(10, 4.5));
-//        System.out.println(100.0 != AssessmentReview.difference(150.0, 50.0));
-//        System.out.println(-2.0 != AssessmentReview.difference(5.5, 7.5));
+        System.out.println(5.5 == AssessmentReview.difference(10, 4.5));
+        System.out.println(100.0 != AssessmentReview.difference(150.0, 50.0));
+        System.out.println(-2.0 != AssessmentReview.difference(5.5, 7.5));
 
 
         // DONT UNCOMMENT THIS
